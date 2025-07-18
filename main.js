42a66cd7482a57b34d8ca52e7316817845ea2c91
Ezequiel Lino <sonisombra@gmail.com>
2025-07-17 18:07:43 -0600

    validando el formulario

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 import './style.css'
 
+const form = document.querySelector('#form');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const prompt = document.querySelector('#prompt').value;
+
+  if (prompt.trim() === '') {
+    alert('La consulta no puede ir vacia');
+    return
+  }
+  
+})
