e749ed31a97579e766a0d65b282efbc453fa2a4b
Ezequiel Lino <sonisombra@gmail.com>
2025-08-07 22:59:57 -0600

    generando texto con diferentes modelos

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 import './style.css'
+import { createOpenRouter } from '@openrouter/ai-sdk-provider';
+import { generateText } from 'ai';
+
+const openrouter = createOpenRouter({
+  apiKey: import.meta.env.VITE_OPENROUTER_KEY
+})
 
 const form = document.querySelector('#form');
 
-form.addEventListener('submit', e => {
+form.addEventListener('submit', async e => {
   e.preventDefault();
 
   const prompt = document.querySelector('#prompt').value;
@@ -11,5 +17,14 @@ form.addEventListener('submit', e => {
     alert('La consulta no puede ir vacia');
     return
   }
+
+  const result = await generateText({
+    model: openrouter('google/gemini-2.0-flash-exp:free'),
+    // model:('deepseek/deepseek-r1-0528-qwen3-8b:free'),
+    // model:('meta-llama/llama-3.3-70b-instruct:free'),
+    prompt: prompt
+  })
+
+  console.log(result)
   
 })
